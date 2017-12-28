070117e6361f23331a1373f9034b4f7cd259ee14
Cédric L <cedric@laubacher.io>
2017-12-29 00:25:04 +0100

    Update nginx v1.13.8

--- mediabutler/rootfs/usr/local/bin/run.sh
+++ mediabutler/rootfs/usr/local/bin/run.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+echo "Moving settings file if it doesn't exist..."
+if [ ! -f ${CONFIG_PATH}/default.json ]; then
+  mv /opt/MediaButler/config/default.json $CONFIG_PATH
+fi
+
 echo "Updating permissions..."
-for dir in /opt/MediaButler /etc/s6.d; do
+for dir in /opt/MediaButler /etc/s6.d /config; do
   if $(find $dir ! -user $UID -o ! -group $GID|egrep '.' -q); then
     echo "Updating permissions in $dir..."
     chown -R $UID:$GID $dir
@@ -13,5 +18,8 @@ echo "Done updating permissions."
 
 sed -i 's/TOKEN_HERE/'"$TOKEN"'/g' /opt/MediaButler/settings.json
 sed -i 's/PREFIX_HERE/'"$PREFIX"'/g' /opt/MediaButler/settings.json
+sed -i 's#PATH_HERE#'"$CONFIG_PATH"'#g' /opt/MediaButler/settings.json
 
-/bin/s6-svscan /etc/s6.d
+sed -i 's/"prefix": "!",/"prefix": "'$PREFIX'",/' "$CONFIG_PATH"/default.json
+
+su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
