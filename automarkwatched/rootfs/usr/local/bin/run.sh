0e3e53b3b2c95ce576ddef13272582632c60c9c2
Cédric L <cedric@laubacher.io>
2018-04-06 12:33:43 +0200

    Remove one RUN

--- automarkwatched/rootfs/usr/local/bin/run.sh
+++ automarkwatched/rootfs/usr/local/bin/run.sh
@@ -9,6 +9,8 @@ fi
 
 sed -i -e '/SECRET_KEY/c\SECRET_KEY = '\'`cat /config/secretkey`\' /opt/AutoMarkWatched/automarkwatched/settings.py
 
+python3 /opt/AutoMarkWatched/manage.py migrate
+
 echo "Updating permissions..."
 for dir in /opt/AutoMarkWatched /etc/s6.d /config; do
   if $(find $dir ! -user $UID -o ! -group $GID|egrep '.' -q); then
