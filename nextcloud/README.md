8aeb8af5a94dccf9e1d6817b9d9372ef866633fd
Cédric Laubacher <cedric@laubacher.io>
2021-02-23 16:42:46 +0100

    Add optional notify_push service

--- nextcloud/README.md
+++ nextcloud/README.md
@@ -9,10 +9,16 @@
 - Based on Alpine Linux.
 - Automatic installation using environment variables.
 - system cron task running.
+- optional [notify_push](https://github.com/nextcloud/notify_push) app
 
 ### Tags
 - **latest** : latest stable version
 
 ### Configuration
 
-You can use the environmental variables described ![here](https://github.com/nextcloud/docker#auto-configuration-via-environment-variables)
+You can use the environmental variables described [here](https://github.com/nextcloud/docker#auto-configuration-via-environment-variables)
+
+#### Custom Variables
+- **ENABLE_PUSH**: 0
+  - [notify_push](https://github.com/nextcloud/notify_push) can be enabled by setting ENABLE_PUSH=1
+  - Remark: Redis is required for this functionality
---
+++ nextcloud/entrypoint_ext.sh
@@ -0,0 +1,37 @@
+installed=0
+run_as 'php /var/www/html/occ status' | grep -q "installed: true" || installed=$?
+
+if [ "${ENABLE_PUSH:-0}" -eq 1 ] && [ $installed -eq 0 ]; then
+    installed_version="$(php -r 'require "/var/www/html/version.php"; echo implode(".", $OC_Version);')"
+    echo "Installed version: $installed_version"
+  
+    if version_greater "$installed_version" "21.0.0.0"; then
+        run_as 'php /var/www/html/occ app:list' | sed -n "/Enabled:/,/Disabled:/p" > /tmp/list_installed
+
+        if grep -q "notify_push" /tmp/list_installed; then
+            echo "notify_push already installed and enabled"
+        else
+            echo "Installing and enabling notify_push"
+            run_as 'php /var/www/html/occ app:install notify_push'
+            run_as 'php /var/www/html/occ app:enable notify_push'
+            # bug with v0.1.2
+            chmod +x /var/www/html/custom_apps/notify_push/bin/*/notify_push
+        fi
+
+        rm /tmp/list_installed
+
+        sed -i '/^autostart=/c\autostart=true' /supervisord.conf
+
+        if grep -q "#include" /etc/nginx/nginx.conf; then
+            sed -i '/#include /c\       include /etc/nginx/nginx_push.conf;' /etc/nginx/nginx.conf
+        fi
+
+    else
+        echo "notify_push only runs on Nextcloud 21 or higher"
+        sed -i '/^autostart=/c\autostart=false' /supervisord.conf
+    fi
+else
+    sed -i '/^autostart=/c\autostart=false' /supervisord.conf
+fi
+
+exec "$@"
