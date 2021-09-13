5c9abe4c553d1905090da289dd403809132ed750
Cédric Laubacher <cedric@laubacher.io>
2021-09-13 16:18:06 +0200

    Enable HEIC preview again in Nextcloud

---
+++ nextcloud/preview.config.php
@@ -0,0 +1,16 @@
+<?php
+$CONFIG = array (
+    'enabledPreviewProviders' => [
+        'OC\Preview\PNG',
+        'OC\Preview\JPEG',
+        'OC\Preview\GIF',
+        'OC\Preview\BMP',
+        'OC\Preview\XBitmap',
+        'OC\Preview\HEIC',
+        'OC\Preview\MP3',
+        'OC\Preview\TXT',
+        'OC\Preview\MarkDown',
+        'OC\Preview\OpenDocument',
+        'OC\Preview\Krita',
+    ],
+);
